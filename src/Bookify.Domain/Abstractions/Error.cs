fdcd0e9d09d6fe4eff9f933ff596d2a08ae1ceef
Regi Shehi <regi_shehi@hotmail.com>
2025-01-01 18:39:35 +0100

    Add docker compose and controllers

--- src/Bookify.Domain/Abstractions/Error.cs
+++ src/Bookify.Domain/Abstractions/Error.cs
@@ -4,4 +4,5 @@ public record Error(string Code, string Name)
 {
     public static readonly Error None = new(string.Empty, string.Empty);
     public static readonly Error NullValue = new("Error.NullValue", "Null value was provided");
+    public static readonly Error NotFound = new("Error.NotFound", "A 'Not Found' error has occurred");
 }
