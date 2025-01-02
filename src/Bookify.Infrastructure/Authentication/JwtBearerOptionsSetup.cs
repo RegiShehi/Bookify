39e438eb5977570cb5743187f13743d1ccb8c9ec
Regi Shehi <regi_shehi@hotmail.com>
2025-01-02 19:06:53 +0100

    Add uniform responses for 401 and 403 errors

--- src/Bookify.Infrastructure/Authentication/JwtBearerOptionsSetup.cs
+++ src/Bookify.Infrastructure/Authentication/JwtBearerOptionsSetup.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+﻿using System.Net;
+using System.Text.Json;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 
 namespace Bookify.Infrastructure.Authentication;
@@ -23,5 +27,42 @@ internal sealed class JwtBearerOptionsSetup : IConfigureNamedOptions<JwtBearerOp
     public void Configure(string? name, JwtBearerOptions options)
     {
         Configure(options);
+
+        options.Events = new JwtBearerEvents
+        {
+            OnChallenge = context =>
+            {
+                // Prevent default response
+                context.HandleResponse();
+
+                var problemDetails = new ProblemDetails
+                {
+                    Status = (int)HttpStatusCode.Unauthorized,
+                    Type = "Unauthorized Error",
+                    Title = "Unauthorized",
+                    Detail = "Authentication is required to access this resource."
+                };
+
+                context.Response.StatusCode = problemDetails.Status.Value;
+                context.Response.ContentType = "application/problem+json";
+                var json = JsonSerializer.Serialize(problemDetails);
+                return context.Response.WriteAsync(json);
+            },
+            OnForbidden = context =>
+            {
+                var problemDetails = new ProblemDetails
+                {
+                    Status = (int)HttpStatusCode.Forbidden,
+                    Type = "ForbiddenError",
+                    Title = "Forbidden",
+                    Detail = "You do not have permission to access this resource."
+                };
+
+                context.Response.StatusCode = problemDetails.Status.Value;
+                context.Response.ContentType = "application/problem+json";
+                var json = JsonSerializer.Serialize(problemDetails);
+                return context.Response.WriteAsync(json);
+            }
+        };
     }
 }
