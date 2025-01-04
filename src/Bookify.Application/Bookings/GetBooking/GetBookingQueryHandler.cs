16cd0d123d2f52141ace0adf635252e1c617419c
Regi Shehi <regi_shehi@hotmail.com>
2025-01-04 10:30:49 +0100

    Add resource based authorization

--- src/Bookify.Application/Bookings/GetBooking/GetBookingQueryHandler.cs
+++ src/Bookify.Application/Bookings/GetBooking/GetBookingQueryHandler.cs
@@ -1,11 +1,13 @@
 ﻿using Bookify.Application.Abstractions.Data;
 using Bookify.Application.Abstractions.Messaging;
+using Bookify.Application.Authentication;
 using Bookify.Domain.Abstractions;
+using Bookify.Domain.Bookings;
 using Dapper;
 
 namespace Bookify.Application.Bookings.GetBooking;
 
-internal sealed class GetBookingQueryHandler(ISqlConnectionFactory sqlConnectionFactory)
+internal sealed class GetBookingQueryHandler(ISqlConnectionFactory sqlConnectionFactory, IUserContext userContext)
     : IQueryHandler<GetBookingQuery, BookingResponse>
 {
     public async Task<Result<BookingResponse>> Handle(GetBookingQuery request, CancellationToken cancellationToken)
@@ -38,6 +40,9 @@ internal sealed class GetBookingQueryHandler(ISqlConnectionFactory sqlConnection
         {
             request.BookingId
         });
+
+        if (booking is null || booking.UserId != userContext.UserId)
+            return Result.Failure<BookingResponse>(BookingErrors.NotFound);
 
         return booking;
     }
