781d4aa8e27f7d33b1aed2cf8c14fb97919c70f7
Regi Shehi <regi_shehi@hotmail.com>
2025-01-04 14:50:17 +0100

    Add structured logging

--- src/Bookify.Api/Extensions/ApplicationBuilderExtensions.cs
+++ src/Bookify.Api/Extensions/ApplicationBuilderExtensions.cs
@@ -18,4 +18,11 @@ public static class ApplicationBuilderExtensions
     {
         app.UseMiddleware<ExceptionHandlingMiddleware>();
     }
+
+    public static IApplicationBuilder UseRequestContextLogging(this IApplicationBuilder app)
+    {
+        app.UseMiddleware<RequestContextLoggingMiddleware>();
+
+        return app;
+    }
 }
