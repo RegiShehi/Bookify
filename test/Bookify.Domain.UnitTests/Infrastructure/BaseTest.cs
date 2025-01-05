5e31d3fe7eb0542b52c6cc1e3de7f747de20ab54
Regi Shehi <regi_shehi@hotmail.com>
2025-01-05 17:32:35 +0100

    Add domain unit tests

---
+++ test/Bookify.Domain.UnitTests/Infrastructure/BaseTest.cs
@@ -0,0 +1,16 @@
+﻿using Bookify.Domain.Abstractions;
+
+namespace Bookify.Domain.UnitTests.Infrastructure;
+
+public abstract class BaseTest
+{
+    protected static T AssertDomainEventWasPublished<T>(Entity entity)
+        where T : IDomainEvent
+    {
+        var domainEvent = entity.GetDomainEvents().OfType<T>().SingleOrDefault();
+
+        if (domainEvent == null) throw new Exception($"{typeof(T).Name} was not published");
+
+        return domainEvent;
+    }
+}
---
+++ test/Bookify.Domain.UnitTests/Users/UserTests.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics;
+using Bookify.Domain.UnitTests.Infrastructure;
+using Bookify.Domain.Users;
+using Bookify.Domain.Users.Events;
+using FluentAssertions;
+
+namespace Bookify.Domain.UnitTests.Users;
+
+public class UserTests : BaseTest
+{
+    [Fact]
+    public void Create_Should_SetPropertyValues()
+    {
+        // arrange
+
+        // act
+        var user = User.Create(UserData.FirstName, UserData.LastName, UserData.Email);
+
+        // assert
+        user.FirstName.Should().Be(UserData.FirstName);
+        user.LastName.Should().Be(UserData.LastName);
+        user.Email.Should().Be(UserData.Email);
+    }
+
+    [Fact]
+    public void Create_Should_RaiseUserCreatedDomainEvent()
+    {
+        // arrange
+
+        // act
+        var user = User.Create(UserData.FirstName, UserData.LastName, UserData.Email);
+
+        // assert
+        var domainEvent = AssertDomainEventWasPublished<UserCreatedDomainEvent>(user);
+
+        domainEvent.UserId.Should().Be(user.Id);
+    }
+
+    [Fact]
+    public void Create_Should_AddRegisteredRoleToUser()
+    {
+        // Act
+        var user = User.Create(UserData.FirstName, UserData.LastName, UserData.Email);
+
+        // Assert
+        user.Roles.Should().Contain(Role.Registered);
+    }
+}
