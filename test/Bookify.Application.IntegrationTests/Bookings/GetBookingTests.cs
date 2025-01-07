337731d241d71654d2ba5badae0f3b9862d886c2
RegiShehi <shehi.regi@gmail.com>
2025-01-07 18:18:45 +0100

    Add GetBooking test

---
+++ test/Bookify.Application.IntegrationTests/Bookings/GetBookingTests.cs
@@ -0,0 +1,28 @@
+﻿using Bookify.Application.Bookings.GetBooking;
+using Bookify.Application.IntegrationTests.Infrastructure;
+using Bookify.Domain.Bookings;
+using FluentAssertions;
+
+namespace Bookify.Application.IntegrationTests.Bookings;
+
+public class GetBookingTests : BaseIntegrationTest
+{
+    private static readonly Guid BookingId = Guid.NewGuid();
+    
+    public GetBookingTests(IntegrationTestWebAppFactory factory) : base(factory)
+    {
+    }
+    
+    [Fact]
+    public async Task GetBooking_ShouldReturnFailure_WhenBookingIsNotFound()
+    {
+        // arrange
+        var command = new GetBookingQuery(BookingId);
+
+        // act
+        var result = await Sender.Send(command);
+
+        // assert
+        result.Error.Should().Be(BookingErrors.NotFound);
+    }
+}
